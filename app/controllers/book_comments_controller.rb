df51570757ca679607b4e6a10402c0c6978d3e01
EC2 Default User <ec2-user@ip-172-31-61-147.ec2.internal>
2021-06-02 21:21:26 +0000

    [Finish]Phase２-task3

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,17 @@
 class BookCommentsController < ApplicationController
+
   def create
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
-    comment.save
+    if comment.save
     redirect_to book_path(book.id)
+    else
+      redirect_to request.referer 
+    end  
   end
-
   def destroy
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.find_by(book_id: book.id)
+    comment = current_user.book_comments.find_by(book_id: params[:book_id])
     comment.destroy
     redirect_to request.referer
   
